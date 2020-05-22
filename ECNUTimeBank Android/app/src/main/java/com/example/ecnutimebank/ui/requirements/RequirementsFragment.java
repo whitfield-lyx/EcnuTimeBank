2c919d98c02675cca2ed85ae9036d94cc847bacf
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-05-22 17:13:17 +0800

    update: Requirement-完成带有searchView的toolbar

--- ECNUTimeBank Android/app/src/main/java/com/example/ecnutimebank/ui/requirements/RequirementsFragment.java
+++ ECNUTimeBank Android/app/src/main/java/com/example/ecnutimebank/ui/requirements/RequirementsFragment.java
@@ -1,14 +1,23 @@
 package com.example.ecnutimebank.ui.requirements;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -17,19 +26,47 @@ import com.example.ecnutimebank.R;
 public class RequirementsFragment extends Fragment {
 
     private RequirementsViewModel requirementsViewModel;
+    private SearchView mSearchView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        requirementsViewModel =
-                ViewModelProviders.of(this).get(RequirementsViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_requirements, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        requirementsViewModel.getText().observe(this, new Observer<String>() {
+        setHasOptionsMenu(true);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        Toolbar toolbar = activity.findViewById(R.id.toolbar_requirements);
+        toolbar.setTitle("Requirements");
+        activity.setSupportActionBar(toolbar);
+        ActionBar actionBar = activity.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.requirement_toolbar_menu, menu);
+        MenuItem search = menu.findItem(R.id.anime_search);
+        mSearchView = (SearchView) search.getActionView();
+        mSearchView.setSubmitButtonEnabled(true);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public boolean onQueryTextSubmit(String query) {
+                Log.i("Requirements", query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
-        return root;
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
