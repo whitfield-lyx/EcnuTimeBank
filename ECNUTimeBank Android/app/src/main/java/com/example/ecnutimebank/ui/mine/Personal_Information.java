6de6b8cb7cca29e98dff7c2b50424220e619a1aa
zhouxingyu88 <578842914@qq.com>
2020-05-24 14:00:29 +0800

    update: 添加所有java类

---
+++ ECNUTimeBank Android/app/src/main/java/com/example/ecnutimebank/ui/mine/Personal_Information.java
@@ -0,0 +1,25 @@
+package com.example.ecnutimebank.ui.mine;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ecnutimebank.R;
+
+public class Personal_Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal_info_layout);
+        ImageView backToAboutMe = findViewById(R.id.MI_backToAboutMe_imageView);
+        backToAboutMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
