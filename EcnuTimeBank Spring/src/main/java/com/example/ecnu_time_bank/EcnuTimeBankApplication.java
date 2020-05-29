24a365deff63e0a971fd535d334be2bbf5b267e9
whitfield-lyx <10842089@qq.com>
2020-05-29 21:21:07 +0800

    后端部分初步

---
+++ EcnuTimeBank Spring/src/main/java/com/example/ecnu_time_bank/EcnuTimeBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecnu_time_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcnuTimeBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcnuTimeBankApplication.class, args);
+    }
+
+}
