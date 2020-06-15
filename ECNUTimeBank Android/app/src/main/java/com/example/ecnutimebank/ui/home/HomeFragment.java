240dcb4f290976f49d6062a33bf84586bbb6ee83
whitfield-lyx <10842089@qq.com>
2020-06-15 17:07:54 +0800

    整理滚动图片函数

--- ECNUTimeBank Android/app/src/main/java/com/example/ecnutimebank/ui/home/HomeFragment.java
+++ ECNUTimeBank Android/app/src/main/java/com/example/ecnutimebank/ui/home/HomeFragment.java
@@ -79,52 +79,9 @@ public class HomeFragment extends Fragment implements  OnRefreshListener, OnLoad
            }
        });
 
-        List<AdPageInfo> dataTest = new ArrayList<>();
-        AdPageInfo info1 = new AdPageInfo("华师大闵行校区", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590330696394&di=d6cc0e9ea89965a3785a91a257ae7410&imgtype=0&src=http%3A%2F%2Fstatic3.loupan.com%2Fnewsimg%2Fimage%2F2019%2F0613%2F1518513473890.jpg%3FimageView2%2F3%2Fw%2F800%2Fh%2F600%2Fq%2F75", "", 2);
-        AdPageInfo info2 = new AdPageInfo("华师大中北校区", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590330696394&di=b18bf60119e8f2831ce7f3599004089e&imgtype=0&src=http%3A%2F%2Fimgbdb2.bendibao.com%2Fshanghai%2F20141%2F8%2F20141815944759.jpg", "", 1);
-        dataTest.add(info1);
-        dataTest.add(info2);
-
+       //设置滚动图片
         mAdPlayBanner = view.findViewById(R.id.place_banner);
-
-        mAdPlayBanner
-                .setImageLoadType(GLIDE)
-                .setOnPageClickListener(new AdPlayBanner.OnPageClickListener() {
-                    @Override
-                    public void onPageClick(AdPageInfo info, int position) {
-                        Intent intent = new Intent(activity, HomePlaceDetailActivity.class);
-                        intent.putExtra("description", "这是一个设施的详细信息。");
-                        startActivity(intent);
-                    }
-                })
-                .setAutoPlay(true)
-                .setIndicatorType(POINT_INDICATOR)
-                .setNumberViewColor(0xcc00A600, 0xccea0000, 0xffffffff)
-                .setInterval(3000)
-                .setImageViewScaleType(AdPlayBanner.ScaleType.CENTER_CROP)
-                //.addTitleView(new TitleView(getContext()).setPosition(PARENT_BOTTOM).setTitlePadding(5, 5, 5, 5).setTitleMargin(0, 0, 0, 25).setTitleSize(15).setViewBackground(0x55000000).setTitleColor(getResources().getColor(R.color.white)))
-                //.addTitleView(TitleView.getDefaultTitleView(getApplicationContext()))
-                .setBannerBackground(0xff565656)
-                .setPageTransformer(new ZoomOutPageTransformer())
-                .setInfoList(dataTest)
-                // 设置不可以手动滑动
-                .setCanScroll(true)
-                .setOnPagerChangeListener(new AdPlayBanner.OnPagerChangeListener() {
-                    @Override
-                    public void onPageSelected(int position) {
-                    }
-
-                    @Override
-                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-                    }
-
-                    @Override
-                    public void onPageScrollStateChanged(int state) {
-
-                    }
-                })
-                .setUp();
+        initAdPlayer();
 
    /*通知滚动条
         marqueeView = view.findViewById(R.id.marqueeView);
@@ -204,4 +161,50 @@ public class HomeFragment extends Fragment implements  OnRefreshListener, OnLoad
         startActivity(intent);
     }
 
+    public void initAdPlayer(){
+        List<AdPageInfo> dataTest = new ArrayList<>();
+        AdPageInfo info1 = new AdPageInfo("华师大闵行校区", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590330696394&di=d6cc0e9ea89965a3785a91a257ae7410&imgtype=0&src=http%3A%2F%2Fstatic3.loupan.com%2Fnewsimg%2Fimage%2F2019%2F0613%2F1518513473890.jpg%3FimageView2%2F3%2Fw%2F800%2Fh%2F600%2Fq%2F75", "", 2);
+        AdPageInfo info2 = new AdPageInfo("华师大中北校区", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1590330696394&di=b18bf60119e8f2831ce7f3599004089e&imgtype=0&src=http%3A%2F%2Fimgbdb2.bendibao.com%2Fshanghai%2F20141%2F8%2F20141815944759.jpg", "", 1);
+        dataTest.add(info1);
+        dataTest.add(info2);
+
+        mAdPlayBanner
+                .setImageLoadType(GLIDE)
+                .setOnPageClickListener(new AdPlayBanner.OnPageClickListener() {
+                    @Override
+                    public void onPageClick(AdPageInfo info, int position) {
+                        Intent intent = new Intent(activity, HomePlaceDetailActivity.class);
+                        intent.putExtra("description", "这是一个设施的详细信息。");
+                        startActivity(intent);
+                    }
+                })
+                .setAutoPlay(true)
+                .setIndicatorType(POINT_INDICATOR)
+                .setNumberViewColor(0xcc00A600, 0xccea0000, 0xffffffff)
+                .setInterval(3000)
+                .setImageViewScaleType(AdPlayBanner.ScaleType.CENTER_CROP)
+                //.addTitleView(new TitleView(getContext()).setPosition(PARENT_BOTTOM).setTitlePadding(5, 5, 5, 5).setTitleMargin(0, 0, 0, 25).setTitleSize(15).setViewBackground(0x55000000).setTitleColor(getResources().getColor(R.color.white)))
+                //.addTitleView(TitleView.getDefaultTitleView(getApplicationContext()))
+                .setBannerBackground(0xff565656)
+                .setPageTransformer(new ZoomOutPageTransformer())
+                .setInfoList(dataTest)
+                // 设置不可以手动滑动
+                .setCanScroll(true)
+                .setOnPagerChangeListener(new AdPlayBanner.OnPagerChangeListener() {
+                    @Override
+                    public void onPageSelected(int position) {
+                    }
+
+                    @Override
+                    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+                    }
+
+                    @Override
+                    public void onPageScrollStateChanged(int state) {
+
+                    }
+                })
+                .setUp();
+    }
 }
