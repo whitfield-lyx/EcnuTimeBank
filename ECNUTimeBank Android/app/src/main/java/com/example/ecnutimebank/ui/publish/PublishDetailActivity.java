669a1c7aab85dab1119277528f6a4b1401632c54
whitfield-lyx <10842089@qq.com>
2020-06-16 19:37:48 +0800

    基本实现发布订单功能

--- ECNUTimeBank Android/app/src/main/java/com/example/ecnutimebank/ui/publish/PublishDetailActivity.java
+++ ECNUTimeBank Android/app/src/main/java/com/example/ecnutimebank/ui/publish/PublishDetailActivity.java
@@ -38,6 +38,7 @@ import java.util.List;
 
 public class PublishDetailActivity extends AppCompatActivity {
 
+    private MaterialEditText etTitle;
     private MaterialEditText etName;
     private MaterialEditText etTime;
     private MaterialEditText etTel;
@@ -58,35 +59,32 @@ public class PublishDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//左侧添加一个默认的返回图标
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
 
-        etName=(MaterialEditText)findViewById(R.id.et_name);
-        etAddress=(MaterialEditText)findViewById(R.id.et_address);
-        etDescription =(MaterialEditText)findViewById(R.id.et_description);
-        etTel = (MaterialEditText)findViewById(R.id.et_tel);
-        etTime = (MaterialEditText)findViewById(R.id.et_time);
+        etTitle=findViewById(R.id.et_title);
+        etName= findViewById(R.id.et_name);
+        etAddress= findViewById(R.id.et_address);
+        etDescription = findViewById(R.id.et_description);
+        etTel = findViewById(R.id.et_tel);
+        etTime = findViewById(R.id.et_time);
 
-        type_spinner = (MaterialSpinner) findViewById(R.id.type_spinner);
+        type_spinner = findViewById(R.id.type_spinner);
         type_spinner.setItems("陪聊", "散步", "代买", "打扫", "其他");
-        type_spinner.setOnItemSelectedListener(new MaterialSpinner.OnItemSelectedListener<String>() {
-            @Override public void onItemSelected(MaterialSpinner view, int position, long id, String item) {
-                type_string = item;
-                //Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
-            }
-        });
+        type_spinner.setOnItemSelectedListener((MaterialSpinner.OnItemSelectedListener<String>) (view, position, id, item) -> type_string = item);
     }
 
     private void setListener(){
         bt_publish =findViewById(R.id.publish_finish_btn);
         bt_publish.setOnClickListener(view -> {
+            String title =etTitle.getText().toString().trim();
             String name = etName.getText().toString().trim();
             String time = etTime.getText().toString().trim();
             String telephone = etTel.getText().toString().trim();
             String address = etAddress.getText().toString().trim();
             String description = etDescription.getText().toString().trim();
             String type = type_string.trim();
-            publish(name,time,telephone,address,description,type);
+            publish(title,name,time,telephone,address,description,type);
 
-            //Intent intent = new Intent(PublishDetailActivity.this, BaseActivity.class);
-            //startActivity(intent);
+            Intent intent = new Intent(PublishDetailActivity.this, BaseActivity.class);
+            startActivity(intent);
         });
     }
 
@@ -99,9 +97,10 @@ public class PublishDetailActivity extends AppCompatActivity {
         return true;
     }
 
-    private void publish(String name,String time,String telephone,String address,String description,String type){
+    private void publish(String title,String name,String time,String telephone,String address,String description,String type){
 
         HashMap params = new HashMap<>();
+        params.put("orderTitle",title);
         params.put("orderTime",time);
         params.put("orderType",type);
         params.put("orderAddress",address);
